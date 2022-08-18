deba84bc20fbc9e3d44b425ddfa35fc1561b33b8
irene <102620704+mynameisnotirene@users.noreply.github.com>
2022-08-18 15:07:32 -0700

    Updating assignments/simple-draw/code.js

--- assignments/simple-draw/code.js
+++ assignments/simple-draw/code.js
@@ -45,4 +45,4 @@ drawFilledCircle(width - 1, height - 50, 50, 'gold');
 
 drawFilledRect(5, height - 20, 45, 15, 'orange');
 
-drawRectangle(width/50, height/70, 60, 'red')
+drawRectangle(width/40, height/20, 30, 'red')
