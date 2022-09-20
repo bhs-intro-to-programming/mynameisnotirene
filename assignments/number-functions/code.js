f8a0719666c21e691acb1a868f8d71c6c16c8850
irene <102620704+mynameisnotirene@users.noreply.github.com>
2022-09-20 15:12:13 -0700

    Updating assignments/number-functions/code.js

--- assignments/number-functions/code.js
+++ assignments/number-functions/code.js
@@ -25,3 +25,6 @@ const distance = (a, b) => {
 const manhattanDistance = (a, b, c, d) => {
   return Math.abs(a-c)+Math.abs(b-d)
 }
+const euclideanDistance = (a, b, c, d) => {
+  return Math.sqrt((((Math.abs(a-c))**2)+((Math.abs(b-d))**2))
+}
