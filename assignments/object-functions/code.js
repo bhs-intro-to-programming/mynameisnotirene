3e931ef6515e312d850332939b74789d864c3b60
irene <102620704+mynameisnotirene@users.noreply.github.com>
2022-11-08 15:10:19 -0800

    Updating assignments/object-functions/code.js

--- assignments/object-functions/code.js
+++ assignments/object-functions/code.js
@@ -7,9 +7,9 @@ const point = (x, y) => {
 const emptyObject = (object) => {
   return {}
 }
-const distance = (x, y) => {
+const distance = (x1, y2) => {
   return 
 }
 const midpoint = (x1, x2) => {
-  return x1/x2
+  return (x1.x + x2.x) - (x1.y + x2.y)
 }
