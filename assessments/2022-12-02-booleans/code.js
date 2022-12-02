57402fd7658f913f65445dddf46b6f729dc06dcd
irene <102620704+mynameisnotirene@users.noreply.github.com>
2022-12-02 14:50:56 -0800

    Updating assessments/2022-12-02-booleans/code.js

--- assessments/2022-12-02-booleans/code.js
+++ assessments/2022-12-02-booleans/code.js
@@ -1,4 +1,7 @@
 const needHeavyCoat = (goOutside, cold) => {
-  return goOutside && cold
+  return goOutside || cold
+}
+const needSunscreen = (goToBeach, goToSki) => {
+  return goToBeach && goToSki
 }
 const 
